a670731e417df2873cac4db9ea8246cc2058ec58
Jack-Pettigrew <jak.pettigrew@gmail.com>
2022-07-25 23:25:29 +0100

    added help command

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 // Require the necessary discord.js classes
-const Discord = require('discord.js');
-const { token } = require('./config.json');
+const Discord = require("discord.js");
+const { token } = require("./config.json");
 
 // Create a new client instance
-const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
+const client = new Discord.Client({
+  intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
+});
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+	console.log("Bot Ready!!");
+	
+	client.application?.commands.create({
+		name: "help",
+		description: "Replies with some help!",	  
+	})
+});
+
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isCommand()) {
+		return;
+	}
+
+	const { commandName, options } = interaction;
+
+	switch (commandName) {
+		case 'help':
+			interaction.reply({
+				content: "This is some extremely unhelpful text!",
+				ephemeral: true,
+			})
+			break;
+	}
 });
 
 // Login to Discord with your client's token
