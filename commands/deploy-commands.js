4443c2bcd44be798f2bfa4c0497d79867e338612
Jack Pettigrew <jak.pettigrew@gmail.com>
2025-01-24 13:35:41 +0000

    Updated deploy commands file

--- commands/deploy-commands.js
+++ commands/deploy-commands.js
@@ -1,5 +1,7 @@
-const { REST, Routes, SlashCommandBuilder } = require("discord.js");
-const { token, applicationID, guildID } = require("../config.json");
+import { REST, Routes, SlashCommandBuilder } from "discord.js";
+import { config } from "dotenv";
+
+config();
 
 /**
  * Array of all possible commands
@@ -8,9 +10,11 @@ const commands = [
   new SlashCommandBuilder()
     .setName("help")
     .setDescription("Replies with some help!"),
+
   new SlashCommandBuilder()
     .setName("darkdax")
     .setDescription("Replies with some DarkDax links!"),
+
   new SlashCommandBuilder()
     .setName("annoy")
     .setDescription("Annoy a server member until further notice!")
@@ -20,6 +24,7 @@ const commands = [
         .setDescription("Annoy a server member until further notice!")
         .setRequired(true);
     }),
+
   new SlashCommandBuilder()
     .setName("stopannoy")
     .setDescription("Stop annoying a server member!")
@@ -29,9 +34,11 @@ const commands = [
         .setDescription("The ID of the user to stop annoying!")
         .setRequired(true);
     }),
+
   new SlashCommandBuilder()
     .setName("randomuser")
     .setDescription("Picks a random user from the server!"),
+
   new SlashCommandBuilder()
     .setName("announcestream")
     .setDescription("Announce a stream time")
@@ -53,30 +60,28 @@ const commands = [
         .setDescription("The announcement message")
         .setRequired(true)
     ),
+
   new SlashCommandBuilder()
     .setName("setstreamlink")
     .setDescription("Sets the link to be shared for stream related commands")
     .addStringOption((option) => option.setName("link").setDescription("The link of the stream location").setRequired(true)),
+    
 ].map((command) => command.toJSON());
 
-const rest = new REST({ version: 10 }).setToken(token);
+const rest = new REST().setToken(process.env.TOKEN);
 
 /**
  * Sets the bot's slash (/) commands
  */
-const setupCommands = async () => {
-  try {
-    console.log("Started setting application (/) commands.");
+try {
+  console.log("Started setting application (/) commands.");
 
-    await rest
-      .put(Routes.applicationCommands(applicationID, guildID), {
-        body: commands,
-      })
-      .then(() => console.log("Successfully set application (/) commands."))
-      .catch(console.error);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-module.exports.setupCommands = setupCommands;
+  await rest
+    .put(Routes.applicationCommands(process.env.CLIENTID, process.env.SERVERID), {
+      body: commands,
+    })
+    .then(() => console.log("Successfully set application (/) commands."))
+    .catch(console.error);
+} catch (error) {
+  console.error(error);
+}
